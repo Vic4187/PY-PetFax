74d3a093c68fd9d482dcee8649bf38277f8b178d
Victor Estrada <vic4187@gmail.com>
2024-06-23 16:17:12 -0500

    Integrate databases

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,13 +1,15 @@
-from  flask import ( Blueprint, render_template )
+from flask import ( Blueprint, render_template )
 import json
 
-pets = json.load(open("pets.json"))
+pets = json.load(open('pets.json'))
 
 bp = Blueprint('pet', __name__, url_prefix="/pets")
 
-#Pets Routes
-
-#Pets Index
 @bp.route('/')
 def index():
-    return render_template("index.html", pets=pets)
+    return render_template('pets/index.html', pets=pets)
+
+@bp.route('/<int:id>')
+def show(id):
+    pet = pets[id - 1]
+    return render_template('pets/show.html', pet=pet)
